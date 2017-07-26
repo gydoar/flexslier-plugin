62c83e3ada342fa09d7549738b98c59fc7183a35
Andres Vega <gydoar@hotmail.com>
2017-07-26 11:57:00 -0500

    Primer commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Flexslider plugin
+Plugin URI: https://github.com/gydoar/flexslier-plugin
+Description: Plugin creado con Flexslider para WordPress
+Version: 1.0
+Author: ANDRES DEV
+Author URI: https://andres-dev.com
+License: GPL2
+*/
+?>
